efa0e7a22d8d33d751eef946a7564fb742c8592d
DESKTOP-VQRQMUI\LENOVO <hyeonmyeong.back@lemonhc.com>
2024-09-16 18:34:16 +0900

    Initial commit

---
+++ src/test/java/whitewise/tobyspring/TobyspringApplicationTests.java
@@ -0,0 +1,13 @@
+package whitewise.tobyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
